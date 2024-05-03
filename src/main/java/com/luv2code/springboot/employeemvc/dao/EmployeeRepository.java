623feb3415f33fff4530897e161c7263d38c1727
kanishk-jain-99 <kanishk.99jain@gmail.com>
2024-05-03 19:53:05 +0530

    SPRING MVC EMPLOYEE PROJECT

--- src/main/java/com/luv2code/springboot/employeemvc/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/employeemvc/dao/EmployeeRepository.java
@@ -3,8 +3,14 @@ package com.luv2code.springboot.employeemvc.dao;
 import com.luv2code.springboot.employeemvc.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    //sort by last name JPA repo will automatically create the query as per the method name given below
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
