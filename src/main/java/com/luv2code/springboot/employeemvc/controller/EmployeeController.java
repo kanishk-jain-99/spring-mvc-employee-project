ca6742ce2f24572b86026eb54c57a344b09880d3
kanishk-jain-99 <kanishk.99jain@gmail.com>
2024-05-03 20:12:06 +0530

    SPRING MVC EMPLOYEE PROJECT

--- src/main/java/com/luv2code/springboot/employeemvc/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/employeemvc/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import com.luv2code.springboot.employeemvc.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,11 +44,31 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showUpdateForm(@RequestParam("employeeId") int theId , Model theModel){
+
+
+        Employee theEmployee = employeeService.findById(theId);
+
+        theModel.addAttribute("employee", theEmployee);
+
+
+        return "employees/employee-form";
+    }
+
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
 
         employeeService.save(theEmployee);
 
+        return "redirect:/employees/list";
+
+    }
+
+    @GetMapping("/delete")
+    public String showUpdateForm(@RequestParam("employeeId") int theId){
+
+        employeeService.deleteById(theId);
         return "redirect:/employees/list";
 
     }
